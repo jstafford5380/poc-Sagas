e982ec017464d014ca427f9fc96807386a95cfba
= <=>
2019-03-11 10:14:21 -0700

    Add project files.

---
+++ SagaPoc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SagaPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
